403821f62079b2a36e671c37ec75958e3e6fa242
contr <controlys123@gmail.com>
2025-10-26 21:35:31 +0300

    Removed 'Stack trace'.

--- src/main/java/com/lightre/fullbrightnesstoggle/mixin/GammaOptionBypassMixin.java
+++ src/main/java/com/lightre/fullbrightnesstoggle/mixin/GammaOptionBypassMixin.java
@@ -1,28 +1,31 @@
 package com.lightre.fullbrightnesstoggle.mixin;
 
 import net.minecraft.client.option.SimpleOption;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
+
 @Mixin(SimpleOption.DoubleSliderCallbacks.class)
 public class GammaOptionBypassMixin {
+    @Unique
+    private static final Logger LOGGER = LogManager.getLogger("FullBrightnessToggle");
 
     @Inject(method = "validate(Ljava/lang/Double;)Ljava/util/Optional;", at = @At("HEAD"), cancellable = true)
     private void alwaysValid(Double value, CallbackInfoReturnable<Optional<Double>> cir) {
-        // Gelen değeri logla
-        System.out.println("[FullBrightnessToggle] validate çağrıldı. Gelen değer: " + value);
-
+        LOGGER.info("[FullBrightnessToggle] validate çağrıldı. Gelen değer: {}", value);
         try {
             Optional<Double> result = Optional.ofNullable(value);
-            System.out.println("[FullBrightnessToggle] validate sonucu: " + result);
+            LOGGER.info("[FullBrightnessToggle] validate sonucu: {}", result);
             cir.setReturnValue(result);
         } catch (Exception e) {
-            System.err.println("[FullBrightnessToggle] validate sırasında hata oluştu:");
-            e.printStackTrace(); // Stack trace logla
+            LOGGER.error("[FullBrightnessToggle] validate sırasında hata oluştu", e);
         }
     }
 }
