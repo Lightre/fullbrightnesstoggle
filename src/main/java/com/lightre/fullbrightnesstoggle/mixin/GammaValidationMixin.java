a4b6a11571a1cfe3b8c13dffeafeb740c1250c8b
contr <controlys123@gmail.com>
2025-10-27 21:51:07 +0300

    Bypass gamma validation and codec to allow any value.

--- src/main/java/com/lightre/fullbrightnesstoggle/mixin/GammaValidationMixin.java
+++ src/main/java/com/lightre/fullbrightnesstoggle/mixin/GammaValidationMixin.java
@@ -1,5 +1,7 @@
 package com.lightre.fullbrightnesstoggle.mixin;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import net.minecraft.client.option.SimpleOption;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,21 +13,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
-
 @Mixin(SimpleOption.DoubleSliderCallbacks.class)
-public class GammaOptionBypassMixin {
+public class GammaValidationMixin {
     @Unique
     private static final Logger LOGGER = LogManager.getLogger("FullBrightnessToggle");
 
     @Inject(method = "validate(Ljava/lang/Double;)Ljava/util/Optional;", at = @At("HEAD"), cancellable = true)
     private void alwaysValid(Double value, CallbackInfoReturnable<Optional<Double>> cir) {
-        LOGGER.info("[FullBrightnessToggle] validate çağrıldı. Gelen değer: {}", value);
         try {
-            Optional<Double> result = Optional.ofNullable(value);
-            LOGGER.info("[FullBrightnessToggle] validate sonucu: {}", result);
-            cir.setReturnValue(result);
+            cir.setReturnValue(Optional.ofNullable(value));
         } catch (Exception e) {
-            LOGGER.error("[FullBrightnessToggle] validate sırasında hata oluştu", e);
+            LOGGER.error("[FullBrightnessToggle] validate error", e);
         }
+    }
+
+    @Inject(method = "codec", at = @At("RETURN"), cancellable = true)
+    private void overrideCodec(CallbackInfoReturnable<Codec<Double>> cir) {
+        cir.setReturnValue(
+                Codec.DOUBLE.flatXmap(
+                        DataResult::success,
+                        DataResult::success
+                )
+        );
     }
 }
